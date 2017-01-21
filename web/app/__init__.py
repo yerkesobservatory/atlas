c280cc8277b962421e101ef74c73af8a2bf425d0
Remy Prechelt <rprechelt@gmail.com>
2017-01-21 00:05:03 +0000

    Updated manage script to work with gunicorn

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -21,24 +21,24 @@ def create_app(config_name):
     """
 
     # create app and load config from file
-    app = Flask(__name__, template_folder='templates/')
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    application = Flask(__name__, template_folder='templates/')
+    application.config.from_object(config[config_name])
+    config[config_name].init_app(application)
     
     # initialize flask extensions
-    boostrap.init_app(app)
-    mail.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
+    boostrap.init_app(application)
+    mail.init_app(application)
+    db.init_app(application)
+    login_manager.init_app(application)
 
     # register main routes and error handlers
     # must be imported here to avoid circular dep
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    application.register_blueprint(main_blueprint)
 
     # register authentication routing
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    application.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     # done
-    return app
+    return application
