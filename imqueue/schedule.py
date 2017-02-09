cfff9210ce6423855cc4f796fdf72e5f7fcb0422
Amanda Pagul <amendochka@gmail.com>
2017-02-08 22:08:30 -0800

    Updated schedule.py

--- imqueue/schedule.py
+++ imqueue/schedule.py
@@ -1,15 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import typing
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, get_sun
 
-
-def schedule(sessions: list) -> list:
-    """ This function gets given a list of dictionaries representing session
-    objects, and needs to return a ordered/scheduled list of the same objects.
+def scheduler(target_list):
     """
-
-    # example for how to extract fields
-    session = sessions[0] # first imaging session in list
-    target = session['target']
-    exposure_time = session['exposure_time']
-    exposure_count = session['exposure_count']
-    filters = session['filters']
-    session_time = (len(filters) + 1)*exposure_count*exposure_time+90 # total execution time in seconds for session
-    # blah blah
+    This function receives a list of target names and outputs a primary
+    observable target
+    
+    Inputs:
+    -------
+    target_list :str: :list: List containing the names of the targets.
+    
+    Outputs:
+    --------
+    primary_target :str: Name of the highest priority target
+    wait :astropy.Time: Wait time in seconds until optimal observation.
+    It takes the value -1 when the object(s) is not observable.
+    """
+    
+    max_altitude_time = {'target':[], 'altitude':[], 'time':[], 'wait':[]}   
+    
+    
+    seo = EarthLocation(lat=38.2886*u.deg, lon=-122.50400*u.deg, height=60*u.m)    
+    
+    obs_time = Time.now()
+    delta_obs_time = np.linspace(0, 18, 1000)*u.hour
+    times = obs_time+delta_obs_time
+    frame = AltAz(obstime=times, location=seo)
+    
+    sun_altaz = get_sun(times).transform_to(frame)
+    sundown_time = times[np.where((sun_altaz.alt < -12*u.deg) == True)[0][0]]
+    sunup_time = times[np.where((sun_altaz.alt < -12*u.deg) == True)[0][-1]]
+    
+    for target in target_list:
+        max_altitude_time['target'].append(target)
+        
+        target_coordinates = SkyCoord.from_name(target)
+        target_altaz = target_coordinates.transform_to(frame)
+        if (np.max(target_altaz.alt)) > 40*u.degree:
+            max_altitude_time['altitude'].append(np.max(target_altaz.alt))
+        else:
+            max_altitude_time['altitude'].append(0)
+        
+        aux_time = times[np.argmax(target_altaz.alt)]
+        max_altitude_time['time'].append(aux_time)
+        
+        aux_delta_time = delta_obs_time[np.argmax(target_altaz.alt)]
+        
+        if (times[np.argmax(target_altaz.alt)] > sundown_time)& (times[np.argmax(target_altaz.alt)] < sunup_time):
+            max_altitude_time['wait'].append(aux_delta_time.to(u.second))
+        else:
+            max_altitude_time['wait'].append('-1')
+    
+    primary_target = max_altitude_time['target'][np.argmin(Time(max_altitude_time['time'])-Time.now())]
+    primary_target_id = np.argmin(Time(max_altitude_time['time'])-Time.now())
+    return primary_target, max_altitude_time['wait'][primary_target_id]
