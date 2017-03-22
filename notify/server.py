07c594f8c5abcdaa9553c5be37450123c5b280d3
Remy Prechelt <rprechelt@gmail.com>
2017-03-21 20:20:14 -0500

    Added notification server to send emails via the MQTT broker

--- notify/server.py
+++ notify/server.py
@@ -2,96 +2,70 @@
 the appropriate email address with the time, date, error message, and other relevant information. 
 """
 
-import time
 import typing
-import sys
-import json
-import paho.mqtt.client as mqtt
+import smtplib
+import email
+from templates import mqtt
 
-class NotifyServer(object):
+
+class NotificationServer(mqtt.MQTTServer):
     """ This class represents a server that listens for notification requests on
-    /`shortname`/notify and emails these messages to the destination
+    /seo/notify and emails these messages to the destination
     """
-
-    def __init__(self, config: dict):
-        """ This creates a new server listening on the specified port; this does
-        not start the server listening, it just creates the server. start() must
-        be called for the server to be initialized.
+    def __init__(self, config: {str}):
+        """ Initializes mail construct and starts server
         """
 
-        self.log('Creating new notification server...', 'green')
+        # MUST INIT SUPERCLASS FIRST
+        super().__init__(config, "Notification Server")
 
-        # save config file
-        self.config = config
 
-        # mqtt client to handle connection
-        self.client = self.connect()
+        # MUST END WITH start() - THIS BLOCKS
+        self.start()
 
-        
-    def connect(self) -> bool:
-        """ Connect to the MQTT broker and return the MQTT client
-        object.
+
+    def topics(self) -> [str]:
+        """ This function must return a list of topics that you wish the server
+        to subscribe to. i.e. ['/seo/queue'] etc.
         """
 
-        # mqtt client to handle connection
-        client = mqtt.Client()
+        prefix = '/'+self.config['general']['shortname']+'/'
+        return [prefix+'notify']
 
-        # server information
-        host = self.config.get('server').get('host') or 'localhost'
-        port = self.config.get('server').get('mosquitto').get('port') or 1883
-        name = self.config.get('general').get('name') or 'Atlas'
-        email = self.config.get('general').get('email') or 'your system administrator'
-
-        # connect to message broker
-        try:
-            client.connect(host, port, 60)
-            self.log('Successfully connected to '+name, color='green')
-        except:
-            self.log('Unable to connect to '+name+'. Please try again later. '
-                     'If the problem persists, please contact '+email, 'red')
-            print(sys.exc_info())
-            exit(-1)
-
-        return client
     
-
-    def log(self, msg: str, color: str = 'white') -> bool:
-        """ Prints a log message to STDOUT. Returns True if successful, False
-        otherwise.
+    def process_message(self, msg: {str}) -> bool:
+        """ This function is given a JSON dictionary message from the broker
+        and must decide how to process the message given the servers purpose. This
+        is automatically called whenever a message is received
         """
-        colors = {'red':'31', 'green':'32', 'blue':'34', 'cyan':'36',
-                  'white':'37', 'yellow':'33', 'magenta':'34'}
-        logtime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
-        log = '\033[1;'+colors[color]+'m'+logtime+' SERVER: '+msg+'\033[0m'
-        print(log)
+
+        if msg['action'] == 'email' and msg.get('to') is not None:
+            self.send_email(msg)
+        else:
+            self.log('Unknown message received', color='magenta')
+
         return True
 
 
-    def process_message(self, client, userdata, msg) -> list:
-        """ This function is called whenever a message is received.
+    def send_email(self, msg: {str}) -> bool:
+        """ Sends email given the parameters in msg. 
         """
-        self.send_email(msg.payload.decode())
-        self.log(msg.topic+": "+msg.payload.decode())
+        s=smtplib.SMTP(self.config['mail']['server'])
 
-    def send_email(msg):
-        """ Parse the decoded message and send the contents to the appropriate
-        person
-        """
-        dest = msg.get('dest') or self.config.get('general').get('email')
-        subject = msg.get('subject') or self.config.get('general').get('subject')
-        message = msg.get('message')
+        # construct the message
+        message = email.message.EmailMessage()
+        message.set_content(msg.get('content'))
+        message['Subject'] = msg.get('subject')
+        message['From'] = self.config.get('mail').get('username')
+        message['To'] = msg.get('to')
 
-        # TODO: Send email!
+        # make a TLS connection
+        s.starttls()
 
+        # login using provided username and password
+        s.login(self.config['mail']['username'], self.config['mail']['password'])
 
-    def run(self):
-        """ Starts the servers listening for new requests; server blocks
-        on the specified port until it receives a request
-        """
-        self.client.on_message = self.process_message
-        name = '/'+self.config.get('general').get('shortname') or 'atlas'
-        self.client.subscribe(name+'/notify')
-        self.client.loop_forever()
-
-
-
+        # send the email and quit
+        s.send_message(message)
+        s.quit()
+        
