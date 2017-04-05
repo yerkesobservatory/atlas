daa7aeeeae9c27a9d7320b469ce5560c39d7da29
Amanda Pagul <amendochka@gmail.com>
2017-04-05 13:04:21 -0400

    Update schedule.py
    proper time frame to calculate sunset/sunrise; better altitude rendering

--- routines/schedule.py
+++ routines/schedule.py
@@ -26,15 +26,21 @@ def schedule(target_list: [str], endtime):
     seo = EarthLocation(lat=38.2886*u.deg, lon=-122.50400*u.deg, height=60*u.m)    
     
     obs_time = Time.now()
-    delta_obs_time = np.linspace(0, 18, 1000)*u.hour
+    delta_obs_time = np.linspace(0, 15, 1000)*u.hour
     times = obs_time+delta_obs_time
     frame = AltAz(obstime=times, location=seo)
     
-    sun_altaz = get_sun(times).transform_to(frame)
-    sundown_time = times[np.where((sun_altaz.alt < -12*u.deg) == True)[0][0]]
-    sunup_time = times[np.where((sun_altaz.alt < -12*u.deg) == True)[0][-1]]
+    start_time = str(Time.now())[:10]+" 00:00:00"
+    fixed_start = Time(start_time, scale="utc")
+    delta_fixed_start = np.linspace(0,15,1000)*u.hour
+    fixed = fixed_start+delta_fixed_start
+    altazframe = AltAz(obstime=fixed, location=seo)  
+    sun_altaz = get_sun(fixed).transform_to(altazframe)
     
-    for target in target_list:
+    sundown_time = fixed[np.where((sun_altaz.alt < -12*u.deg) == True)[0][0]]
+    sunup_time = fixed[np.where((sun_altaz.alt < -12*u.deg) == True)[0][-1]]
+    
+    for i,target in enumerate(target_list):
         
         target_ra = target[1]
         target_dec = target[2]
@@ -51,28 +57,32 @@ def schedule(target_list: [str], endtime):
         if (np.max(target_altaz.alt)) > 40*u.degree:
             max_altitude_time['altitude'].append(np.max(target_altaz.alt))
         else:
-            max_altitude_time['altitude'].append(0)
+            max_altitude_time['altitude'].append(0*u.degree)
         
         aux_time = times[np.argmax(target_altaz.alt)]
         max_altitude_time['time'].append(aux_time)
         
         aux_delta_time = delta_obs_time[np.argmax(target_altaz.alt)]
-        
-        if (times[np.argmax(target_altaz.alt)] > sundown_time) & (times[np.argmax(target_altaz.alt)] < sunup_time) & (times[np.argmax(target_altaz.alt)] < Time(endtime,scale='utc')):
+    
+        if (max_altitude_time['altitude'][i]>0*u.degree) & (times[np.argmax(target_altaz.alt)] > sundown_time)\
+        & (times[np.argmax(target_altaz.alt)] < sunup_time) & (times[np.argmax(target_altaz.alt)] < Time(endtime,scale='utc')):
             max_altitude_time['wait'].append(aux_delta_time.to(u.second))
         else:
-            max_altitude_time['wait'].append('-1')
+            max_altitude_time['wait'].append(-1*u.s)
+            
+    max_altitude_time['time']=np.array(max_altitude_time['time'])
+    good_object = np.array([max_altitude_time['wait'][itgt]>-1*u.s for itgt in range(len(max_altitude_time['wait']))])
     
-    primary_target = max_altitude_time['target'][np.argmin(Time(max_altitude_time['time'])-Time.now())]
-    primary_target_id = np.argmin(Time(max_altitude_time['time'])-Time.now())
-
-    for target in target_list:
-        if target[0] == primary_target:
-            if type(max_altitude_time['wait'][primary_target_id]) is not str:
-                return target, int(max_altitude_time['wait'][primary_target_id].value)
-            else:
-                return target, int(max_altitude_time['wait'][primary_target_id])
-
-    self.log("Scheduler couldn't pick an object")
-    return (-1, -1, -1), -1
-
+    if np.count_nonzero(good_object)>0:
+        if np.count_nonzero(good_object)>1:
+            aux_id = np.argmin(Time(max_altitude_time['time'][good_object])-Time.now())
+            primary_target_id = np.where(good_object)[0][aux_id]
+            primary_target = np.array(max_altitude_time['target'])[primary_target_id]
+        else:
+            primary_target_id = np.where(good_object)[0][0] 
+            primary_target = np.array(max_altitude_time['target'])[primary_target_id]
+    else:
+        self.log("Scheduler couldn't pick an object")
+        return (-1, -1, -1), -1
+    
+    return target, max_altitude_time['wait'][primary_target_id]
