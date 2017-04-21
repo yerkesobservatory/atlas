38a84a18aa1e425a503d66e4944831052d3de9ad
Remy Prechelt <rprechelt@gmail.com>
2017-04-21 17:45:39 -0500

    Added old executor to new codebase

---
+++ imqueue/executor.py
@@ -0,0 +1,451 @@
+## This file implements the execution of telescope queues; at the scheduled time,
+## it loads in the queue file for tonight's imaging and executes each request
+
+import json
+import time
+import datetime
+
+import ming
+import sqlalchemy
+
+import telescope
+from templates import base
+from telescope import telescope
+from routines import schedule, target, pinpoint
+from config import config
+from .exception import *
+
+
+class Executor(base.AtlasServer):
+    """ This class is responsible for executing and scheduling a
+    list of imaging sessions stored in the queue constructed by
+    the queue server.
+    """
+
+    def __init__(self):
+        """ This creates a new executor to execute a single nights
+        list of sessions stored in the JSON file specified by filename.
+        """
+
+        # MUST INIT SUPERCLASS FIRST
+        super().__init__('Executor')
+        self.slack('The queue executor has started up...', '#queue')
+
+        # dummy telescope variable
+        self.telescope = None
+
+        # TODO: Connect to monogo DB
+        self.log('Succesfully connected to the queue database')
+
+        # take numbias*exposure_count biases
+        self.numbias = config.queue.numbias
+
+        # directory to store images on telescope controller
+        self.remote_dir = config.queue.remote_dir
+
+        # MUST END WITH start() - THIS BLOCKS
+        self.start()
+
+
+    def topics(self) -> [str]:
+        """ Returns the topics the server will be subscribed to.
+                
+        This function must return a list of topics that you wish the server
+        to subscribe to. i.e. ['/atlas/queue'] etc.
+        """
+
+        return ['/'+config.general.shortname+'/executor']
+
+    
+    def process_message(self, topic: str, msg: dict) -> None:
+        """ This function is given a dictionary message from the broker
+        and must decide how to process the message. 
+        """
+        if msg.get('type') == 'start':
+            self.execute_queue()
+        else:
+            self.log('Executor received unknown message')
+        
+
+    def load_sessions(self) -> ['Sessions']:
+        """ This function returns a list of all session objects
+        in the database that have not been executed
+        """
+        # TODO
+
+        return sessions
+
+
+    def wait_until_good(self) -> bool:
+        """ Wait until the weather is good for observing.
+        Waits 10 minutes between each trial. Cancels execution
+        if weather is bad for 4 hours.
+        """
+        self.log('Waiting until weather is good...')
+        elapsed_time = 0 # total elapsed wait time
+        max_wait = config.queue.max_wait_time
+
+        # get weather from telescope
+        weather = self.telescope.weather_ok()
+        while weather is False:
+
+            # sleep for 10 minutes
+            self.log('Executor is sleeping for 15 minutes...')
+            self.slack('Executor is sleeping for 15 minutes', '#queue')
+            time.sleep(60*config.queue.wait_time) # time to sleep in seconds
+            elapsed_time += (60*config.queue.wait_time)
+
+            # shut down after 4 hours of continuous waiting
+            if elapsed_time >= (60*60*max_wait):
+                self.log('Bad weather for {} hours. Shutting down the queue...'.format(max_wait),
+                         color='magenta')
+                self.slack('Bad weather for {} hours. Shutting down the queue...'.format(max_wait),
+                           '#queue')
+                raise WeatherException
+
+            # update weather
+            weather = self.telescope.weather_ok()
+
+        self.log('Weather is currently good.')
+        self.slack('Weather is currently good.', '#queue')
+        return True
+
+    
+    def execute_queue(self) -> bool:
+        """ Executes the list of session objects for this queue.
+        """
+        self.slack('Starting execution of the queue...', '#queue')
+
+        # instantiate telescope object for control
+        self.telescope = telescope.Telescope(self.config, dryrun=self.dryrun)
+        self.log('Executor has connection to telescope')
+
+        # wait for the weather to be good
+        try:
+            self.wait_until_good()
+        except WeatherException:
+            return False
+        except Exception as e:
+            self.log('Unknown exception occured while waiting for weather...', color='red')
+            self.slack('Unknown exception occured while waiting for weather...', '#queue')
+
+        # open telescope
+        self.log('Opening telescope dome...')
+        self.slack('Opening telescope dome...', '#queue') 
+        self.telescope.open_dome()
+        self.telescope.enable_tracking()
+        self.telescope.keep_open(3600)
+
+        # load queues from database
+        self.sessions = self.load_sessions()
+        self.slack('Queue has {} unexecuted sessions'.format(len(self.sessions)), '#queue')
+
+        # default endtime - queue_start + 8 hours
+        # TODO: Draw this from database
+        endtime = datetime.datetime.today() + datetime.timedelta(hours=8)
+
+        # we build an (id, ra, dec) list
+        self.log('Building RA,Dec coordinates for queue')
+        objects = []
+        for s in self.sessions:
+            # convert observations to ra/dec
+            try:
+                ra, dec = target.find_target(s.target)
+                objects.append((s.id, ra, dec))
+            except Exception as e:
+                self.log('find_target: '+str(e))
+                self.log('find_target unable to find object. Skipping...', color='magenta')
+                self.slack('find_target unable to find object. Skipping...', '#queue')
+                continue
+
+        # iterate over session list
+        while len(self.sessions) != 0:
+
+            # default location
+            location = ''
+
+            # schedule remaining sessions
+            self.log('Calling the scheduler...')
+            (objid, ra, dec), wait = schedule.schedule(objects, endtime=endtime)
+
+            # if the scheduler returns None, we are done
+            if objid == -1:
+                self.log('Scheduler returned no object. Quitting the executor...')
+                break
+
+            # find object by id
+            found_sessions = list(filter(lambda x: x.id == objid, self.sessions))
+
+            # make sure we found something - this should only be length one
+            if len(found_sessions) != 1:
+                self.log('We were unable to find a session by that ID', color='magenta')
+                self.log(str(found_sessions))
+                continue
+
+            # pick session
+            session = found_sessions[0]
+            self.log('Scheduler has selected {}'.format(session))
+            self.slack('Scheduler has selected {}'.format(session),
+                       '#queue')
+
+            # strip the whitespace from the names
+            session.target = session.target.strip().replace(' ', '')
+            
+            # check whether we need to wait before executing
+            if wait > 0:
+                self.log('Sleeping for {} seconds as requested by scheduler'.format(wait))
+                self.slack('Sleeping for {} seconds as requested by scheduler'.format(wait), '#queue')
+                if wait > 10*60:
+                    if self.telescope.dome_open() is True:
+                        self.log('Closing down the telescope while we sleep')
+                        self.telescope.close_down()
+                time.sleep(wait)
+
+            # check whether every session executed correctly
+            self.log('Executing session for {}'.format(session.user.email or 'none'), color='blue')
+            self.slack('Executing session for {}'.format(session.user.email or 'none'), '#queue')
+            try:
+                # execute session
+                location = self.execute(session, ra, dec)
+
+                # succesful execution
+                if location != '':
+
+                    # send remote path to pipeline for async processing
+                    msg = {'type':'process', 'location':location}
+                    self.slack('Sending location to pipeline: {}...'.format(location), '#queue')
+                    self.client.publish('/seo/pipeline', json.dumps(msg))
+
+                    # set the session as completed
+                    session.executed = True
+                    self.dbsession.commit()
+                    self.log('Completed executing {}.'.format(session))
+                    self.slack('Completed executing {} for {}.'.format(session.target,
+                                                                      session.user.email), '#queue')
+
+                # remove the session from the remaining sessions
+                self.sessions.remove(session)
+
+                # remove the object
+                obj = list(filter(lambda x: x[0] == objid, objects))[0]
+                objects.remove(obj)
+
+
+            except Exception as e:
+                self.log('Error while executing session {}'.format(session.id),
+                         color='red')
+                self.slack('An exception occured while executiong session {}'.format(session.id), '#queue')
+                self.log('start: '+str(e), color='red')
+                self.finish()
+                exit(1)
+                
+        # close down
+        self.log('Finished executing the queue! Closing down...', color='green')
+        self.slack('Finished executing the queue! Closing down...', '#queue')
+        self.finish()
+
+        return True
+
+    
+    def execute(self, session: dict, ra: str, dec: str) -> str:
+        """ Execute a single imaging session. If successful, returns
+        directory where files are stored on telescope control server.
+        """
+
+        # calculate base file name
+        date = time.strftime('%Y-%m-%d', time.gmtime())
+        username = session.user.email.split('@')[0]
+        dirname = self.remote_dir+'/'+'_'.join([date, username, session.target, str(session.id)])
+
+        # try and execute
+        try:
+            self.wait_until_good()
+            if self.telescope.dome_open() is False:
+                self.telescope.open_dome()
+            # point telescope at target
+            self.log('Slewing to {}'.format(session.target))
+            if self.telescope.goto(ra, dec) is False:
+                self.log('Object is not currently visible. Skipping...', color='magenta')
+                return ''
+
+            # create directory
+            self.log('Making directory to store observations on telescope server...')
+            self.telescope.make_dir(dirname)
+            self.slack('Succesfully made directory to store files: {}'.format(dirname), '#queue')
+
+            basename = dirname+'/'+'_'.join([date, username, session.target])
+
+            # we should be pointing roughly at the right place
+            # now we pinpoint
+            self.log('Starting telescope pinpointing...')
+            good_pointing = pinpoint.point(ra, dec, self.telescope)
+
+            # let's check that pinpoint did not fail
+            if good_pointing is False:
+                self.log('Pinpoint failed!')
+                self.slack('Pinpoint failed!', '#queue')
+
+            # extract variables
+            exposure_time = session.exposure_time
+            exposure_count = session.exposure_count
+            binning = session.binning
+
+            # for each filter
+            self.slack('Taking science exposures...', '#queue')
+            filters = self.parse_filters(session)
+            for filt in filters:
+
+                # check weather - wait until weather is good
+                self.wait_until_good()
+                
+                # if the telescope has randomly closed, open up
+                if self.telescope.dome_open() is False:
+                    self.slack('Dome closed unexpectedly. Opening up', '#queue')
+                    self.telescope.open_dome()
+
+                # check our pointing with pinpoint again
+                self.log('Re-pinpointing telescope...')
+                pinpoint.point(ra, dec, self.telescope)
+
+                # reenable tracking
+                self.telescope.enable_tracking()
+
+                # take exposures!
+                self.take_exposures(basename, exposure_time, exposure_count, binning, filt)
+
+            # reset filter back to clear
+            self.log('Switching back to clear filter')
+            self.telescope.change_filter('clear')
+
+            # take exposure_count darks
+            self.slack('Taking dark frames...', '#queue')
+            self.take_darks(basename, exposure_time, exposure_count, binning)
+
+            # take numbias*exposure_count biases
+            self.slack('Taking bias frames...', '#queue')
+            self.take_biases(basename, exposure_time, exposure_count, binning, self.numbias)
+
+            # return the directory containing the files
+            return dirname
+
+        except Exception as e:
+            self.log('The executor has encountered a fatal error. Please manually'
+                     'close down the telescope.', 'red')
+            self.slack('The executor has encountered a fatal error. Please manually'
+                       'close down the telescope. \n {}'.format(e), '#queue')
+            self.log('execute: '+str(e), color='red')
+            self.finish()
+            return None
+
+        
+    def parse_filters(self, session) -> [str]:
+        """ Parse a session objects boolean session
+        fields and return a list of strings representing
+        the filters to be used for the session. 
+        """
+        filters = []
+        if session.filter_i is True:
+            filters.append('i')
+        if session.filter_r is True:
+            filters.append('r')
+        if session.filter_g is True:
+            filters.append('g')
+        if session.filter_u is True:
+            filters.append('u')
+        if session.filter_z is True:
+            filters.append('z')
+        if session.filter_ha is True:
+            filters.append('h-alpha')
+        if session.filter_clear is True:
+            filters.append('clear')
+
+        return filters
+
+    
+    def take_exposures(self, basename: str, exp_time: int,
+                       count: int, binning: int, filt: str) -> bool:
+        """ Take count exposures, each of length exp_time, with binning, using the filter
+        filt, and save it in the file built from basename.
+        """
+        # change to that filter
+        self.log('Switching to {} filter'.format(filt))
+        self.telescope.change_filter(filt)
+
+        # take exposure_count exposures
+        i = 0
+        while i < count: 
+
+            # create image name
+            filename = basename+'_'+filt+'_'+str(exp_time)+'s'
+            filename += '_bin'+str(binning)+'_'+str(i)
+            self.log('Taking exposure {}/{} with name: {}'.format(i+1, count, filename))
+
+            # take exposure
+            self.telescope.take_exposure(filename, exp_time, binning)
+
+            # if the telescope has randomly closed, open up and repeat the exposure
+            if self.telescope.dome_open() is False:
+                self.log('Slit closed during exposure - repeating previous exposure!', color='magenta')
+                self.slack('Slit closed during exposure - repeating previous exposure!', '#queue')
+                self.wait_until_good()
+                self.telescope.open_dome()
+                continue
+            else: # this was a sucessful exposure - take the next one
+                i += 1
+
+        return True
+
+
+    def take_darks(self, basename: str, exp_time: int, count: int, binning: int) -> bool:
+        """ Take a full set of dark frames for a given session. Takes exposure_count
+        dark frames.
+        """
+        for numdark in range(0, count):
+            # create file name
+            filename = basename+'_dark_'+str(exp_time)+'s'
+            filename += '_bin'+str(binning)+'_'+str(numdark)
+            self.log('Taking dark {}/{} with name: {}'.format(numdark+1, count, filename))
+
+            self.telescope.take_dark(filename, exp_time, binning)
+
+        return True
+
+
+    def take_biases(self, basename: str, exp_time: int,
+                    count: int, binning: int, numbias: int) -> bool:
+        """ Take the full set of biases for a given session.
+        This takes exposure_count*numbias biases
+        """
+
+        # create file name for biases
+        biasname = basename+'_'+str(exp_time)+'s'
+        biasname += '_bin'+str(binning)
+        self.log('Taking {} biases with names: {}'.format(count*numbias, biasname))
+
+        # take numbias*exposure_count biases
+        for nb in range(0, count*numbias):
+            self.telescope.take_bias(biasname+'_'+str(nb), binning)
+
+        return True
+
+    
+    def finish(self):
+        """ Close the executor in event of success of failure; closes the telescope, 
+        closes ssh connection, closes log file
+        """
+        if self.telescope is not None:
+            self.slack('Closing down the telescope and disconnecting...', '#queue')
+            self.telescope.close_down()
+            self.telescope.disconnect()
+
+        return 
+
+        
+    def close(self):
+        """ This function is called when the server receives a shutdown
+        signal (Ctrl+C) or SIGINT signal from the OS. Use this to close
+        down open files or connections. 
+        """
+        self.finish()
+        
+        return
