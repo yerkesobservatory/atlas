88e0a3827319166d53932e57707244b66517da76
Remy Prechelt <rprechelt@gmail.com>
2017-05-04 14:35:13 -0500

    Starting template for observation class

--- db/observation.py
+++ db/observation.py
@@ -1,5 +1,34 @@
 import pymodm
+from pymodm import fields
+
 
 class Observation(pymodm.MongoModel):
-    pass
+
+    # unique primary id of this object
+    id = fields.ObjectIdField(primary_key=True)
+
+    # the target name or RA/DEC
+    target = fields.CharField(min_length=2)
+
+    # the time for each exposure
+    exposure_time = fields.FloatField(min_value=0, max_value=1800)
+
+    # the number of exposures of this object
+    exposure_count = fields.IntegerField(min_value=1, max_value=100)
+
+    # the filters to be used
+    filters = fields.ListField(field=CharField)
+
+    # the date it was submitted
+    submit_date = fields.DateTimeField()
+
+    # has this been executed
+    executed = fields.BooleanField()
+    
+    # the date it was executed
+    exec_date = fields.DateTimeField()
+
+    
+
+    
     
