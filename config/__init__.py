5f241103670c23ad0412eaaf6cffb69303e62822
Remy Prechelt <rprechelt@gmail.com>
2017-05-10 12:22:47 -0500

    Added 'with' wrapper around config reading

--- config/__init__.py
+++ config/__init__.py
@@ -2,10 +2,9 @@ import toml
 from . import config as conf
 
 # open and read toml file
-config_file = open('config/config.toml')
+with open('config/config.toml') as config_file:
+    # parse into Python dict
+    config_dict = toml.load(config_file)
 
-# parse into Python dict
-config_dict = toml.load(config_file)
-
-# create Config class
-config = conf.Config(config_dict)
+    # create Config class
+    config = conf.Config(config_dict)
