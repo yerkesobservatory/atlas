b24ce95da407d6a1911cc5483a3882ed4a25a8b3
Remy Prechelt <rprechelt@gmail.com>
2017-07-26 05:12:14 -0500

    Updates to telescope classes in __init__

--- telescope/__init__.py
+++ telescope/__init__.py
@@ -2,16 +2,7 @@ from . import server
 from . import ssh_telescope
 from . import remote_telescope
 
-def Telescope(username: str, host: str, method: str = 'remote'):
-    """ A wrapper around SSHTelescope and RemoteTelescope classes. 
-    """
-    if method == 'remote':
-        return remote_telescope.RemoteTelescope(username, host)
-    elif method == 'ssh':
-        return ssh_telescope.SSHTelescope()
-    else:
-        print("Unknown telescope type")
-        
+Telescope = remote_telescope.RemoteTelescope
 SSHTelescope = ssh_telescope.SSHTelescope
 RemoteTelescope = remote_telescope.RemoteTelescope
 TelescopeServer = server.TelescopeServer
