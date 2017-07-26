d0501419205594c1b821487d26ff262cf406d062
Remy Prechelt <rprechelt@gmail.com>
2017-07-26 04:52:49 -0500

    Updates to parsing TOML files into Python object

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,17 @@
 import toml
-from . import config as conf
+
+class Config(object):
+    """ Load a TOML file into a set of nested Python Config() objects. 
+    """
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+            
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return Config(value) if isinstance(value, dict) else value
 
 # open and read toml file
 with open('config/config.toml') as config_file:
@@ -7,4 +19,4 @@ with open('config/config.toml') as config_file:
     config_dict = toml.load(config_file)
 
     # create Config class
-    config = conf.Config(config_dict)
+    config = Config(config_dict)
