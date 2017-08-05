f372ea6b96d3641e42a1a86972a6a5674429847f
Remy Prechelt <rprechelt@gmail.com>
2017-08-05 03:39:46 -0500

    Removed RemoteTelescope and moved it to atlas-client

--- telescope/__init__.py
+++ telescope/__init__.py
@@ -1,8 +1,6 @@
 from . import server
 from . import ssh_telescope
-from . import remote_telescope
 
-Telescope = remote_telescope.RemoteTelescope
 SSHTelescope = ssh_telescope.SSHTelescope
-RemoteTelescope = remote_telescope.RemoteTelescope
+Telescope = SSHTelescope
 TelescopeServer = server.TelescopeServer
