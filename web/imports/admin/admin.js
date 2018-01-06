bd9d61ec7370ddace89d276a2d8ecffb33c2bf68
Remy Prechelt <rprechelt@gmail.com>
2018-01-06 12:20:02 +0000

    Added modal to edit user profile and change passwords from the admin portal

--- web/imports/admin/admin.js
+++ web/imports/admin/admin.js
@@ -14,92 +14,95 @@ Template.adminObservations.onCreated(function onCreated() {
 // access observations
 Template.adminObservations.helpers({
     observations() {
-	return Observations.find();
-    }, 
+        return Observations.find();
+    },
     settings() {
-	return {
-	    collection: Observations,
-	    showRowCount: true,
-	    showNavigationRowsPerPage: false,
-	    noDataTmpl: Template.noObservations, 
-	    fields: [
-		{key: 'email',
-		 label: 'User',
-		},
-		{key: 'program',
-		 label: 'Program',
-		 fn: function (value, object, key) {
-		     return Programs.find(value).fetch()[0].name;
-		 }}, 
-		{key: 'target',
-		 label: 'Target'},
-		{key: 'exposure_time',
-		 label: 'Exposure Time (s)'},
-		{key: 'exposure_count',
-		 label: 'Exposure Count'},
-		{key: 'filters',
-		 label: 'Filters',
-		 fn: function (value, object, key) {
-		     return value.join(', ');
-		 }}, 
-		{key: 'binning',
-		 label: 'Binning'},
-		// {key: 'submitDate',
-		//  label: 'Date Submitted'}, 
-		{key: 'completed',
-		 label: 'Completed',
-		 fn: function (value, object, key) {
-		     if (value === true) {
-			 return "Yes";
-		     } else {
-			 return "No";
+        return {
+            collection: Observations,
+            showRowCount: true,
+            showNavigationRowsPerPage: false,
+            noDataTmpl: Template.noObservations,
+            fields: [
+                {key: 'email',
+                 label: 'User',
+                },
+                {key: 'program',
+                 label: 'Program',
+                 fn: function (value, object, key) {
+		     program = Programs.findOne(value);
+		     if (program) {
+			 return program.name;
 		     }
-		 }
-		},
-		{label: '',
-		 tmpl: Template.observationAction
-		}
-	    ]
-	};
-    }
-});
+                }},
+            {key: 'target',
+             label: 'Target'},
+            {key: 'exposure_time',
+             label: 'Exposure Time (s)'},
+            {key: 'exposure_count',
+             label: 'Exposure Count'},
+            {key: 'filters',
+             label: 'Filters',
+             fn: function (value, object, key) {
+                 return value.join(', ');
+             }},
+            {key: 'binning',
+             label: 'Binning'},
+            // {key: 'submitDate',
+            //  label: 'Date Submitted'},
+            {key: 'completed',
+             label: 'Completed',
+             fn: function (value, object, key) {
+                 if (value === true) {
+                     return "Yes";
+                 } else {
+                     return "No";
+                 }
+             }
+            },
+            {label: '',
+             tmpl: Template.observationAction
+            }
+        ]
+    };
+}
+				  });
 
 
 Template.adminObservations.events({
     // on press of the action button
     'click .reactive-table tbody tr': function (event) {
-	event.preventDefault();
-	// checks if the actual clicked element has the class `delete`
-	if (event.target.className.includes('action-delete')) {
-	    // delete program
-	    Meteor.call('observations.remove', this._id);
-	} else if (event.target.className.includes('action-completed')) {
-	    // mark program completed
-	    Meteor.call('observations.setCompleted', this._id, ! this.completed);
-	}
+        event.preventDefault();
+        // checks if the actual clicked element has the class `delete`
+        if (event.target.className.includes('action-delete')) {
+            // delete program
+            Meteor.call('observations.remove', this._id);
+        } else if (event.target.className.includes('action-completed')) {
+            // mark program completed
+            Meteor.call('observations.setCompleted', this._id, ! this.completed);
+        }
     }
 });
 
 Template.newAnnouncement.events({
     // on press of Announce button
     'submit .new-announcement'(event) {
-	
-	// prevent browser action
-	event.preventDefault();
 
-	// clear 'success' formatting from form
-	// TODO: Add formatting to form
-	$('.new-announcement').find('.form-group').removeClass('has-success');
+        // prevent browser action
+        event.preventDefault();
 
-	// extract arguments
-	const target = event.target;
-	const title = target.title.value;
-	const text = target.text.value;
+        // clear 'success' formatting from form
+        // TODO: Add formatting to form
+        $('.new-announcement').find('.form-group').removeClass('has-success');
 
-	// submit new announcement
-	Meteor.call('announcements.insert', title, text);
+        // extract arguments
+        const target = event.target;
+        const title = target.title.value;
+        const text = target.text.value;
 
-	// reset form
-	$('.new-announcement')[0].reset();
+        // submit new announcement
+        Meteor.call('announcements.insert', title, text);
+
+        // reset form
+        $('.new-announcement')[0].reset();
     },
 });
--- web/imports/admin/users.js
+++ web/imports/admin/users.js
@@ -1,4 +1,5 @@
 import './users.html';
+// import '../ui/profile.html';
 
 import { Programs } from '../api/programs.js';
 import { Affiliations } from '../api/users.js';
@@ -12,66 +13,66 @@ Template.adminUsers.onCreated(function onCreated() {
 
 Template.userAction.helpers({
     isAdmin(userId) {
-	return Roles.userIsInRole(userId, 'admin');
+        return Roles.userIsInRole(userId, 'admin');
     },
 });
 
 
 Template.adminUsers.helpers({
-        affiliations() {
-	return Affiliations.find({});
+    affiliations() {
+        return Affiliations.find({});
     },
     settings() {
-	return {
-	    collection: Meteor.users,
-	    showRowCount: true,
-	    showNavigationRowsPerPage: false,
-	    fields: [
-		{key: '',
-		 label: 'Email',
-		 fn: function (value, object, key) {
-		     return object.emails[0].address;
-		 }},
-		// {key: 'firstName',
-		//  label: 'First Name'},
-		// {key: 'lastName',
-		//  label: 'Last Name'},
-		{key: 'profile.affiliation',
-		 label: 'Affiliation', 
-		},
-		{key: 'profile.minor',
-		 label: 'Under 18?',
-		 fn: function (value, object, key) {
-		     if (value === true) {
-			 return "Yes";
-		     } else {
-			 return "No";
-		     }
-		 }
-		},
-            {key: '_id',
-             label: 'ID', 
-            }, 
-		{key: '_id',
-		 label: 'Role',
-		 fn: function (value, object, key) {
-		     // console.log(value);
-		     // console.log(Roles.userIsInRole(value, 'users'))
-		     // if (Roles.userIsInRole(value, 'admins')) {
-		     // 	 return 'Admin';
-		     // }
-		     // else if (Roles.userIsInRole(value, 'users')) {
-		     // 	 return 'User';
-		     // } else {
-		     // 	     return 'Unknown';
-		     // }
-		 }
-		}, 
-		{label: '',
-		 tmpl: Template.userAction
-		}
-	    ]
-	}
+        return {
+            collection: Meteor.users,
+            showRowCount: true,
+            showNavigationRowsPerPage: false,
+            fields: [
+                {key: '',
+                 label: 'Email',
+                 fn: function (value, object, key) {
+                     return object.emails[0].address;
+                 }},
+                {key: 'profile.firstName',
+                 label: 'First'},
+                {key: 'profile.lastName',
+                 label: 'Last'},
+                {key: 'profile.affiliation',
+                 label: 'Affiliation',
+                },
+                {key: 'profile.minor',
+                 label: 'Under 18?',
+                 fn: function (value, object, key) {
+                     if (value === true) {
+                         return "Yes";
+                     } else {
+                         return "No";
+                     }
+                 }
+                },
+                {key: '_id',
+                 label: 'ID',
+                },
+                {key: '_id',
+                 label: 'Role',
+                 fn: function (value, object, key) {
+                     // console.log(value);
+                     // console.log(Roles.userIsInRole(value, 'users'))
+                     // if (Roles.userIsInRole(value, 'admins')) {
+                     //          return 'Admin';
+                     // }
+                     // else if (Roles.userIsInRole(value, 'users')) {
+                     //          return 'User';
+                     // } else {
+                     //              return 'Unknown';
+                     // }
+                 }
+                },
+                {label: '',
+                 tmpl: Template.userAction
+                }
+            ]
+        }
     }
 });
 
@@ -79,80 +80,91 @@ Template.adminUsers.helpers({
 Template.adminUsers.events({
     'submit .new-user'(event) {
 
-	// prevent default submission
-	event.preventDefault();
+        // prevent default submission
+        event.preventDefault();
 
-	// clear 'success' formatting from form
-	$('.new-user').find('.form-group').removeClass('has-success');
+        // clear 'success' formatting from form
+        $('.new-user').find('.form-group').removeClass('has-success');
 
-	// extract values
-	const target = event.target;
-	const email = target.email.value;
-	const affiliation = target.affiliation.value;
-	const minor = target.minor.checked;
+        // extract values
+        const target = event.target;
+        const email = target.email.value;
+        const affiliation = target.affiliation.value;
+        const minor = target.minor.checked;
 
-	// create new user
-	const profile =  {
-		affiliation: affiliation,
-		minor: minor,
-	}
+        // create new user
+        const profile =  {
+            affiliation: affiliation,
+            minor: minor,
+        }
 
-	// insert user
-	Meteor.call('users.insert', email, profile);
+        // insert user
+        Meteor.call('users.insert', email, profile);
 
-	// reset form
-	$('.new-user')[0].reset();
+        // reset form
+        $('.new-user')[0].reset();
 
-    }, 
-    
+    },
+
     // on press of the delete button
     'click .reactive-table tbody tr': function (event) {
-	event.preventDefault();
-	// checks if the actual clicked element has the class `delete`
-	if (event.target.className.includes('action-delete')) {
-	    // delete user
-	    Meteor.call('users.remove', this._id);
-	} else if (event.target.className.includes('make-admin')) {
+        event.preventDefault();
+        // checks if the actual clicked element has the class `delete`
+        if (event.target.className.includes('action-delete')) {
+            // delete user
+            Meteor.call('users.remove', this._id);
+        } else if (event.target.className.includes('make-admin')) {
 
-	    // toggle admin state
-	    Meteor.call('users.addToRole', this._id, 'admin');
-	} else if (event.target.className.includes('remove-admin')) {
+            // toggle admin state
+            Meteor.call('users.addToRole', this._id, 'admin');
+        } else if (event.target.className.includes('remove-admin')) {
 
-	    // toggle admin state
-	    Meteor.call('users.removeFromRole', this._id, 'admin');
-	} 
-	
+            // toggle admin state
+            Meteor.call('users.removeFromRole', this._id, 'admin');
+        }
+	else if (event.target.className.includes('edit-user')) {
+	    // show the modal for the user
+	    const id = this._id;
+	    Modal.show('editProfile', function () {
+		// Under no circumstances, replace `id` below
+		// with this._id(). MAJOR BUG.
+		// See comment by Soitech:
+		// https://github.com/PeppeL-G/bootstrap-3-modal/issues/5
+	    	return Meteor.users.findOne(id);
+	    });
+	}
+
     }
 });
 
 // build rules for form validation
 Template.adminUsers.onRendered(function() {
     $( '.new-user' ).validate({
-	errorClass: 'text-danger',
-	errorElement: 'p',
-	highlight: function(element, errorClass) {
-	    $(element.form).find('#form-'+element.id).removeClass('has-success');
-	    $(element.form).find('#form-'+element.id).addClass('has-error');
-	},
-	unhighlight: function(element, errorClass) {
-	    $(element.form).find('#form-'+element.id).removeClass('has-error');
-	    $(element.form).find('#form-'+element.id).addClass('has-success');
-	},
-	rules: {
-	    email: {
-		required: true,
-		minlength: 5,
-		maxlength: 48},
-	    affiliation: {
-		required: true,
-		minlength: 5,
-		maxlength: 32
-	    }, 
-	},
-	messages: {
-	    email: {
-		required: "Every user needs an email address!",
-	    }
-	}
+        errorClass: 'text-danger',
+        errorElement: 'p',
+        highlight: function(element, errorClass) {
+            $(element.form).find('#form-'+element.id).removeClass('has-success');
+            $(element.form).find('#form-'+element.id).addClass('has-error');
+        },
+        unhighlight: function(element, errorClass) {
+            $(element.form).find('#form-'+element.id).removeClass('has-error');
+            $(element.form).find('#form-'+element.id).addClass('has-success');
+        },
+        rules: {
+            email: {
+                required: true,
+                minlength: 5,
+                maxlength: 48},
+            affiliation: {
+                required: true,
+                minlength: 5,
+                maxlength: 32
+            },
+        },
+        messages: {
+            email: {
+                required: "Every user needs an email address!",
+            }
+        }
     });
 });
