1816485af31935c53c3d74b18e2b0eb564d85582
Remy Prechelt <rprechelt@gmail.com>
2018-01-07 09:54:32 +0000

    Added copy to clipboard for observation links

--- web/imports/ui/observations.js
+++ web/imports/ui/observations.js
@@ -10,86 +10,91 @@ Template.observations.onCreated(function onCreated() {
     Meteor.subscribe('observations');
 });
 
+Template.observationAction.onRendered(function() {
+    var clipboard = new Clipboard('.copy-link');
+});
+
 // subscribe to stream
 Template.newObservation.onCreated(function onCreated() {
     Meteor.subscribe('programs');
 });
 
+
 // access observations
 Template.observations.helpers({
     observations() {
-        return Observations.find({ owner: Meteor.userId()});
+	return Observations.find({ owner: Meteor.userId()});
     },
     settings() {
-        return {
-            collection: Observations,
-            showRowCount: true,
-            showNavigationRowsPerPage: false,
-            noDataTmpl: Template.noObservations,
-            fields: [
-                {key: 'program',
-                 label: 'Program',
-                 fn: function (value, object, key) {
+	return {
+	    collection: Observations,
+	    showRowCount: true,
+	    showNavigationRowsPerPage: false,
+	    noDataTmpl: Template.noObservations,
+	    fields: [
+		{key: 'program',
+		 label: 'Program',
+		 fn: function (value, object, key) {
 		     program = Programs.findOne(value);
 		     if (program) {
 			 return program.name;
 		     }
-                 }},
-                {key: 'target',
-                 label: 'Target'},
-                {key: 'exposure_time',
-                 label: 'Exposure Time (s)'},
-                {key: 'exposure_count',
-                 label: 'Exposure Count'},
-                {key: 'filters',
+		 }},
+		{key: 'target',
+		 label: 'Target'},
+		{key: 'exposure_time',
+		 label: 'Exposure Time (s)'},
+		{key: 'exposure_count',
+		 label: 'Exposure Count'},
+		{key: 'filters',
 		 label: 'Filters',
-                 fn: function (value, object, key) {
-                     return value.join(', ');
-                 }},
-                {key: 'binning',
-                 label: 'Binning'},
-                // {key: 'submitDate',
-                //  label: 'Date Submitted'},
-                {key: 'completed',
-                 label: 'Completed',
-                 fn: function (value, object, key) {
-                     if (value === true) {
-                         return "Yes";
-                     } else {
-                         return "No";
-                     }
-                 }
-                },
-                {label: '',
-                 tmpl: Template.observationAction
-                }
-            ]
-        };
+		 fn: function (value, object, key) {
+		     return value.join(', ');
+		 }},
+		{key: 'binning',
+		 label: 'Binning'},
+		// {key: 'submitDate',
+		//  label: 'Date Submitted'},
+		{key: 'completed',
+		 label: 'Completed',
+		 fn: function (value, object, key) {
+		     if (value === true) {
+			 return "Yes";
+		     } else {
+			 return "No";
+		     }
+		 }
+		},
+		{label: '',
+		 tmpl: Template.observationAction
+		}
+	    ]
+	};
     }
 });
 
 Template.observations.events({
     // on press of the action button
     'click .reactive-table tbody tr': function (event) {
-        event.preventDefault();
-        // checks if the actual clicked element has the class `delete`
-        if (event.target.className.includes('action-delete')) {
-            // delete program
-            Meteor.call('observations.remove', this._id);
-        } else if (event.target.className.includes('action-completed')) {
-            // mark program completed
-            Meteor.call('observations.setCompleted', this._id, ! this.completed);
-        }
+	event.preventDefault();
+	// checks if the actual clicked element has the class `delete`
+	if (event.target.className.includes('action-delete')) {
+	    // delete program
+	    Meteor.call('observations.remove', this._id);
+	} else if (event.target.className.includes('action-completed')) {
+	    // mark program completed
+	    Meteor.call('observations.setCompleted', this._id, ! this.completed);
+	}
     }
 });
 
 // subscribe to observations
 Template.newObservation.helpers({
     programs() {
-        return Programs.find({ owner: Meteor.userId() });
+	return Programs.find({ owner: Meteor.userId() });
     },
     isGeneral(program) {
-        return (program.name == "General");
+	return (program.name == "General");
     },
 });
 
@@ -97,19 +102,19 @@ Template.newObservation.helpers({
 Template.observations.events({
     'submit .new-observation'(event) {
 
-        // prevent default browser
-        event.preventDefault();
+	// prevent default browser
+	event.preventDefault();
 
-        // clear 'success' formattingn from form
-        $('.new-observation').find('.form-group').removeClass('has-success');
+	// clear 'success' formattingn from form
+	$('.new-observation').find('.form-group').removeClass('has-success');
 
-        // get value from form
-        const target = event.target;
-        const progId = target.program.value;
-        const target_name = target.target.value;
-        const exptime = target.exptime.value;
-        const expcount = target.expcount.value;
-        const binning = target.binning.value;
+	// get value from form
+	const target = event.target;
+	const progId = target.program.value;
+	const target_name = target.target.value;
+	const exptime = target.exptime.value;
+	const expcount = target.expcount.value;
+	const binning = target.binning.value;
 	const lunar = target.lunar.value;
 	const airmass = target.airmass.value;
 	const offset_ra = target.offset_ra.value;
@@ -147,8 +152,8 @@ Template.observations.events({
 	// submit new observation
 	Meteor.call('observations.insert', progId, target_name, exptime, expcount, binning, filters, options);
 
-        // reset form
-        $('.new-observation')[0].reset();
+	// reset form
+	$('.new-observation')[0].reset();
 
     },
 });
@@ -156,66 +161,66 @@ Template.observations.events({
 // build rules for form validation
 Template.observations.onRendered(function() {
     $( '.new-observation' ).validate({
-        errorClass: 'text-danger',
-        errorElement: 'p',
-        highlight: function(element, errorClass) {
-            $(element.form).find('#form-'+element.id).removeClass('has-success');
-            $(element.form).find('#form-'+element.id).addClass('has-error');
-        },
-        unhighlight: function(element, errorClass) {
-            $(element.form).find('#form-'+element.id).removeClass('has-error');
-            $(element.form).find('#form-'+element.id).addClass('has-success');
-        },
-        rules: {
-            program: {
-                required: true,
-            },
-            target: {
-                required: true,
-                minlength: 2,
-                maxlength: 18
-            },
-            exptime: {
-                required: true,
-                min: 0.1,
-                max: 900
-            },
-            expcount: {
-                required: true,
-                min:1,
-                max:100,
-                digits: true
-            },
-            binning: {
-                required: true,
-                min:1,
-                max: 8,
-                digits: true
-            }
-        },
-        messages: {
-            target: {
-                required: "Please enter a target for your observation!",
-                minlength: "That doesn't look like a real target...",
-                maxlength: "That's not a valid target name - please enter an identifier i.e. 'M31', 'NGC6946'"
-            },
-            exptime: {
-                required: "We need to know how long you want to expose for!",
-                min: "That exposure time is too short; minimum exposure-time is 0.1s",
-                max: "That exposure time is waaaaay too long; most things will be saturated"
-            },
-            expcount: {
-                required: "Please enter a valid integer nmber of exposure counts",
-                min: "You need to take atleast 1 exposure!",
-                max: "That is an excessive number of exposures; please make this less than 100",
-                digits: "This needs to be an integer - we can't have any half exposures can we?"
-            },
-            binning: {
-                required: "You need to set a binning - we recommend 1 or 2",
-                min: "CCD Binning needs to be greated than 1!",
-                max: "A CCD binning over 8 is excessive - please lower the binning",
-                digits: "This needs to be an integer!"
-            }
-        },
+	errorClass: 'text-danger',
+	errorElement: 'p',
+	highlight: function(element, errorClass) {
+	    $(element.form).find('#form-'+element.id).removeClass('has-success');
+	    $(element.form).find('#form-'+element.id).addClass('has-error');
+	},
+	unhighlight: function(element, errorClass) {
+	    $(element.form).find('#form-'+element.id).removeClass('has-error');
+	    $(element.form).find('#form-'+element.id).addClass('has-success');
+	},
+	rules: {
+	    program: {
+		required: true,
+	    },
+	    target: {
+		required: true,
+		minlength: 2,
+		maxlength: 18
+	    },
+	    exptime: {
+		required: true,
+		min: 0.1,
+		max: 900
+	    },
+	    expcount: {
+		required: true,
+		min:1,
+		max:100,
+		digits: true
+	    },
+	    binning: {
+		required: true,
+		min:1,
+		max: 8,
+		digits: true
+	    }
+	},
+	messages: {
+	    target: {
+		required: "Please enter a target for your observation!",
+		minlength: "That doesn't look like a real target...",
+		maxlength: "That's not a valid target name - please enter an identifier i.e. 'M31', 'NGC6946'"
+	    },
+	    exptime: {
+		required: "We need to know how long you want to expose for!",
+		min: "That exposure time is too short; minimum exposure-time is 0.1s",
+		max: "That exposure time is waaaaay too long; most things will be saturated"
+	    },
+	    expcount: {
+		required: "Please enter a valid integer nmber of exposure counts",
+		min: "You need to take atleast 1 exposure!",
+		max: "That is an excessive number of exposures; please make this less than 100",
+		digits: "This needs to be an integer - we can't have any half exposures can we?"
+	    },
+	    binning: {
+		required: "You need to set a binning - we recommend 1 or 2",
+		min: "CCD Binning needs to be greated than 1!",
+		max: "A CCD binning over 8 is excessive - please lower the binning",
+		digits: "This needs to be an integer!"
+	    }
+	},
     });
 });
