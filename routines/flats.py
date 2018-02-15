720fda11a7f2822581188e5ba24a285913651fa3
Remy Prechelt <rprechelt@gmail.com>
2018-02-15 01:42:28 +0000

    Fixed whitespace formatting in flats

--- routines/flats.py
+++ routines/flats.py
@@ -15,7 +15,7 @@ def take_flats(telescope: 'Telescope') -> bool:
     Parameters
     ----------
     telescope: Telescope
-        A connected telescope object
+        A connected and locked telescope object
 
     Returns
     -------
@@ -25,18 +25,18 @@ def take_flats(telescope: 'Telescope') -> bool:
 
     #where should we put this?
     flats_image_sequence = [
-    #   (bin,   filter,     count)
+        #(bin,   filter,     count)
         (1,     'h-alpha',  5),
-        (2,     'h-alpha',  5),   
+        (2,     'h-alpha',  5),
         (1,     'z-band',  5),
         (1,     'r-band',  5),
         (1,     'i-band',  5),
-        (1,     'g-band',  5), 
+        (1,     'g-band',  5),
         (2,     'z-band',  5),
-        (2,     'r-band',  5),                    
+        (2,     'r-band',  5),
         (1,     'clear',   10),
         (2,     'i-band',  5),
-        (2,     'g-band',  5),         
+        (2,     'g-band',  5),
         (2,     'clear',   10)
     ]
 
@@ -46,7 +46,6 @@ def take_flats(telescope: 'Telescope') -> bool:
     # wait until sun is at max_sun_alt_for_flats (e.g., -1 deg) in 1 minute intervals
     telescope.wait_until_good(config.telescope.max_sun_alt_for_flats, 1)
 
-    # assume telescope is already locked by atlas?
 
     # turn any dome lamps off
     telescope.lamps_off()
@@ -67,7 +66,6 @@ def take_flats(telescope: 'Telescope') -> bool:
 
     # open the observatory
     telescope.open_dome(-1)
-
     telescope.keep_open(3600) #keep dome open for an hour or until told to closedown
 
     # disable tracking
@@ -109,7 +107,7 @@ def take_flats(telescope: 'Telescope') -> bool:
                 mean = telescope.get_mean_image_count(flat_fits_file_prefix+'.fits')
                 if mean < 0:
                     telescope.log.error('There was an error obtaining the mean count of the flat image. Quitting...')
-                    return False;                   
+                    return False;
                 #how close are we to the optimum count for flats?
                 factor = float(config.telescope.optimum_count_for_flats)/float(mean)
                 #calc new exposure time
@@ -118,7 +116,7 @@ def take_flats(telescope: 'Telescope') -> bool:
                     telescope.log.error('The exposure time (%f) is too long (> %f). Quitting...'%(optimum_exposure, config.telescope.max_exposure_for_flats))
                     return False #should we clean up?
                 time.sleep(config.telescope.delay_between_test_flats)
-            #we have our optimum exposure calculated. take a *real* flat
+                #we have our optimum exposure calculated. take a *real* flat
             exposure = optimum_exposure #update exposure
             flatname = 'flat_%s_%.2fsec_bin%d_%s_%s_num%d_seo'%(filter, exposure, binning, config.telescope.username, datetime.datetime.utcnow().strftime('%Y%b%d_%Hh%Mm%Ss'), i)
             if not telescope.take_exposure(flatname, exposure, 1, binning, filter):
@@ -131,13 +129,13 @@ def take_flats(telescope: 'Telescope') -> bool:
             mean = telescope.get_mean_image_count(flatname+'.fits')
             if mean < 0:
                 telescope.log.error('There was an error obtaining the mean count of the flat image. Quitting...')
-                return False; 
+                return False;
             #how close are we to the optimum count for flats?
             factor = float(config.telescope.optimum_count_for_flats)/float(mean)
             #calc new exposure time
             optimum_exposure = factor*config.telescope.exposure_scaling_fudge_for_flats*exposure
             #shimmy the scope pointing
             telescope.goto_point_for_flats()
-    
+
     # all done!
     telescope.close_dome()
