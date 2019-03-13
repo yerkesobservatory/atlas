5f906ef4f00eebd5a19d92c8839500363082bc60
Amanda Pagul <amendochka@gmail.com>
2019-03-13 12:24:23 -0700

    Update coordinates.js
    edited coordinate parser to include solar system planets

--- web/imports/ui/coordinates.js
+++ web/imports/ui/coordinates.js
@@ -1,21 +1,23 @@
 import $ from 'jquery';
-// create a new validator to check for a taget string or valid RA/DEC
+// create a new validator to check for a taget string or valid RA/DEC                                                  
 $.validator.addMethod("validCoordinate", function(value, element) {
     var check_length=0;
-    // if string contains :,+ or - then we assume that it is a RA/Dec string
-    if ((value.indexOf(':') > -1)){// || (value.indexOf('+') > -1) || (value.indexOf('-') > -1)) {
-        // we assume that we have RA/Dec
-        // this checks for the following forms
-        // 00:00:00 +-00:00:00
-        // 00:00:00 +-00:00:00.00
-        // 00:00:00.00 +-00:00:00
-        // 00:00:00.00 +-00:00:00.00
-        // 00h00m00s +-00d00m00s
-        // 00h00m00s +-00d00m00.00s
-        // 00h00m00.00s +-00d00m00s
-        // 00h00m00.00s +-00d00m00.00s
+    var solar_system = ['mercury','venus','moon','mars','jupiter','saturn','uranus','neptune','pluto']
+    // if string contains :,+ or - then we assume that it is a RA/Dec string                                           
+    if ((value.indexOf(':') > -1)){// || (value.indexOf('+') > -1) || (value.indexOf('-') > -1)) {                     
+        // we assume that we have RA/Dec                                                                               
+        // this checks for the following forms                                                                         
+        // 00:00:00 +-00:00:00                                                                                         
+        // 00:00:00 +-00:00:00.00                                                                                      
+        // 00:00:00.00 +-00:00:00                                                                                      
+        // 00:00:00.00 +-00:00:00.00                                                                                   
+        // 00h00m00s +-00d00m00s                                                                                       
+        // 00h00m00s +-00d00m00.00s                                                                                    
+        // 00h00m00.00s +-00d00m00s                                                                                    
+        // 00h00m00.00s +-00d00m00.00s                                                                                 
 
-        re = /\d{1,2}[h:]\d{1,2}[m:]\d{1,2}(?:.\d{1,2}){0,1}s{0,1}\s[+-]\d{1,2}[d:]\d{1,2}[m:]\d{1,2}(?:.\d{1,2}){0,1}s{0,1}/;
+        re = /\d{1,2}[h:]\d{1,2}[m:]\d{1,2}(?:.\d{1,2}){0,1}s{0,1}\s[+-]\d{1,2}[d:]\d{1,2}[m:]\d{1,2}(?:.\d{1,2}){0,1}\
+s{0,1}/;
         if (re.exec(value)) {
             return true;
         }
@@ -23,21 +25,25 @@ $.validator.addMethod("validCoordinate", function(value, element) {
             return false;
         }
 
-        // otherwise we assume we don't understand this coordinate system
+        // otherwise we assume we don't understand this coordinate system                                              
         return false;
-    } else {
+    }
+
+    else if (solar_system.indexOf(String(value).toLowerCase()) >= 0){
+        return true;}
+    else{
         var targetIsValid = false;
         $.ajax({
           type: "GET",
           url: "https://simbad.u-strasbg.fr/simbad/sim-id?output.format=ASCII&Ident="+value,
-          async: false, //this flag makes the call synchronous, i.e. waits for the HTTP response!
+          async: false, //this flag makes the call synchronous, i.e. waits for the HTTP response!                      
           success: function(result){
-            if(result.length > 300) { //300 is the magic number
-                targetIsValid = true;    
+            if(result.length > 300) { //300 is the magic number                                                        
+                targetIsValid = true;
             }
           }
         });
-        return targetIsValid;         
+        return targetIsValid;
     }
 }, "Invalid target name or RA/Dec pair");
 //	else{true_or_false=false;}
